6ee27ef8d696b0fd41b04de41ad0d6ec9345ac3d
Krzysztof Wlazło <59226422+kwlazlo@users.noreply.github.com>
2021-11-14 21:07:46 +0100

    displaying elements arraylist products
    changing struct of code about adding elements to arrayList

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,22 +10,17 @@ public class Main {
     //git push
 
     public static void main(String[] args) {
-        Produkt Bread = new Produkt(1, "Bread", 3.5, 20);
-        Produkt Water = new Produkt(2, "Water", 1, 100);
-        Produkt Milk = new Produkt(3, "Milk", 2, 30);
-        Produkt Chocolate = new Produkt(4, "Chocolate", 4.5, 70);
-        Produkt Candy_Bar = new Produkt(5, "Candy_Bar", 2.5, 40);
-        Produkt Ham = new Produkt(6, "Ham", 7.5, 25);
+
 
         int choice;
         Scanner scanner = new Scanner(System.in);
-        ArrayList Products = new ArrayList();
-        Products.add(Bread);
-        Products.add(Water);
-        Products.add(Milk);
-        Products.add(Chocolate);
-        Products.add(Candy_Bar);
-        Products.add(Ham);
+        List<Produkt> products = new ArrayList();
+        products.add(new Produkt(1, "Bread", 3.5, 20));
+        products.add(new Produkt(2, "Water", 1, 100));
+        products.add(new Produkt(3, "Milk", 2, 30));
+        products.add(new Produkt(4, "Chocolate", 4.5, 70));
+        products.add(new Produkt(5, "Candy_Bar", 2.5, 40));
+        products.add(new Produkt(6, "Ham", 7.5, 25));
 
         do {
             System.out.println("Choose action:\n" +
@@ -38,8 +34,8 @@ public class Main {
                 case 1:
                     System.out.println("Products");
 
-                    for(Produkt product : Products){
-                        System.out.println(product);
+                    for(Produkt produkt : products) {
+                        System.out.println(produkt);
                     }
 
                     break;
@@ -56,3 +52,4 @@ public class Main {
         while(choice != 4);
     }
 }
+
